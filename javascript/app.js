2974eb3979ece66f9b4aa91f608d3fea2296cfb8
dpena001 <david.pena@vitcomp.com>
2017-07-29 00:44:35 -0400

    Added Graph in the Display Function

--- javascript/app.js
+++ javascript/app.js
@@ -87,9 +87,60 @@ function displayStock(response) {
   $("#stockSymbol").empty();
   $("#stockPrice").empty();
   $("#stockDate").empty();
+  $("#chart").empty();
   $("<h3>").attr('class', 'stockNameDisplay').html('Name: ' + response.dataset.name).appendTo("#stockName");
   $("<h4>").attr('class', 'stockSymbolDisplay').html('Stock Symbol/Code: ' + response.dataset.dataset_code).appendTo("#stockSymbol");
   $("<h4>").attr('class', 'stockCurrentPrice').html('Last Closing Price (USD): ' + '$' + response.dataset.data[0][1]).appendTo("#stockPrice");
   $("<h4>").attr('class', 'stockCurrentDate').html('Date: '+ response.dataset.data[0][0]).appendTo("#stockDate");
+  
+  var label =[];
+  var data = [];
+
+  for (var i = 10; i > 0; i--) {
+   
+    label.push(response.dataset.data[i][0]);
+    data.push(response.dataset.data[i][1]);
+  }
+
+    console.log(label);
+    console.log(data);
+
+  var data = {
+      //labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+       labels: label,
+       datasets: [{
+           label: "Price",
+           backgroundColor: "rgba(0, 0, 0, 0)",
+           borderColor: "rgba(0, 57, 122, 1)",
+           borderWidth: 2,
+           hoverBackgroundColor: "rgba(255,99,132,0.4)",
+           hoverBorderColor: "rgba(255,99,132,1)",
+           data: data,
+       }]
+   };
+
+var options = {
+  maintainAspectRatio: false,
+  scales: {
+    yAxes: [{
+      stacked: true,
+      gridLines: {
+        display: true,
+        color: "rgba(0, 0, 255, 0.3)"
+      }
+    }],
+    xAxes: [{
+      gridLines: {
+        display: false
+      }
+    }]
+  }
+};
+
+Chart.Line('chart', {
+  options: options,
+  data: data
+});
+
 }
 
